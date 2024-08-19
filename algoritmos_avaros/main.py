fed8df60488db82cede9296e947c03743aa58b11
Kirill-Makienko-Tkachenko <elchiqvo@gmail.com>
2024-08-19 10:37:02 -0600

    xd

--- algoritmos_avaros/main.py
+++ algoritmos_avaros/main.py
@@ -1,4 +1,3 @@
-from binSearch import RangeMap
 
 # se puede usar el codigo de la presentacion
 # https://inst-fs-iad-prod.inscloudgate.net/files/42d43942-9974-48b2-9339-409504fc35d6/Programaci%C3%B3n%20Din%C3%A1mica%E2%80%8B.pdf?download=1&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MjM3OTEyNjAsInVzZXJfaWQiOiIxNDMyODAwMDAwMDAyODk4NjciLCJyZXNvdXJjZSI6Ii9maWxlcy80MmQ0Mzk0Mi05OTc0LTQ4YjItOTMzOS00MDk1MDRmYzM1ZDYvUHJvZ3JhbWFjaSVDMyVCM24lMjBEaW4lQzMlQTFtaWNhJUUyJTgwJThCLnBkZiIsImp0aSI6ImNmYWY3NTBmLTVhMGMtNDEyMi04YmYwLWZkYjc3YWExOTllZiIsImhvc3QiOiJleHBlcmllbmNpYTIxLnRlYy5teCIsIm9yaWdpbmFsX3VybCI6Imh0dHBzOi8vYTE0MzI4LTE5NTcwMDI5NC5jbHVzdGVyMjExLmNhbnZhcy11c2VyLWNvbnRlbnQuY29tL2NvdXJzZXMvMTQzMjh-NDkxMzkzL2ZpbGVzLzE0MzI4fjE5NTcwMDI5NC9jb3Vyc2UlMjBmaWxlcy9Qcm9ncmFtYWNpJUMzJUIzbiUyMERpbiVDMyVBMW1pY2ElRTIlODAlOEIucGRmP2Rvd25sb2FkX2ZyZD0xXHUwMDI2bm9fY2FjaGU9dHJ1ZVx1MDAyNnJlZGlyZWN0PXRydWVcdTAwMjZzZl92ZXJpZmllcj1leUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpJVXpVeE1pSjkuZXlKMWMyVnlYMmxrSWpvaU1UUXpNamd3TURBd01EQXdNamc1T0RZM0lpd2ljbTl2ZEY5aFkyTnZkVzUwWDJsa0lqb2lNVFF6TWpnd01EQXdNREF3TURBd01EQXhJaXdpYjJGMWRHaGZhRzl6ZENJNkltVjRjR1Z5YVdWdVkybGhNakV1ZEdWakxtMTRJaXdpY21WMGRYSnVYM1Z5YkNJNmJuVnNiQ3dpWm1Gc2JHSmhZMnRmZFhKc0lqb2lhSFIwY0hNNkx5OWxlSEJsY21sbGJtTnBZVEl4TG5SbFl5NXRlQzlqYjNWeWMyVnpMelE1TVRNNU15OW1hV3hsY3k4eE9UVTNNREF5T1RRdlpHOTNibXh2WVdRX1pHOTNibXh2WVdSZlpuSmtQVEZjZFRBd01qWm1ZV3hzWW1GamExOTBjejB4TnpJek9ESXlOVGsySWl3aVpYaHdJam94TnpJek9ESXlPRGsyZlEuTzBFVEFKTTBITW1PN2doS3pzdTRxU3BNVlB1TlpNQ05rLVdNS1JnekhfcUptZ0x6M3lLTWloa09RaTU2OWU5TWVnR0NHcUF4SHc0dXNySkdZc2cyblEiLCJleHAiOjE3MjM4Nzc2NjB9.23Ylen2OT6D6zF3hMigh6MScT4P6zj-eQzKzTGAI4SLPMoRhwC0wes0xo2eMFc22BF_u8sljzXErqxpHdM3QuQ
@@ -7,33 +6,21 @@ from binSearch import RangeMap
 def min_pesos(total, denominations, cambio):
     
     totalIntacto = total
-    # Para poder buscar una llave que sea un entero en un diccionario de python, se tiene que hacer una estructura de dato personalizada, al menos eso encontre en stackoverflow
-    # Cada que se llama la funcion volvemos a asignar el diccionario actualizado
-    
-    # Ya que el metodo __getitem__ lanza una excepcion si no encuentra un dato
-    # Realizamos un try catch, si existe el resultado, lo regresamos, si no, no
     
     pesos_usados = []
     count = 0
     
     
-    if len(cambio) != 1:
-        
-        mapaBuscable = RangeMap(cambio)
-        try:
-            
-            result = mapaBuscable[total]
-            print("Cambio ya habia sido calculado")
-            
-            for item in result:
-                pesos, num_pesos = item
-                count = count + num_pesos
-                pesos_usados.append((pesos, num_pesos))
-                
+    if total in cambio:
+        print("Cambio ya habia sido calculado")
+        result = cambio[total]
+        for item in result:
+            pesos, num_pesos = item
+            count = count + num_pesos
+            pesos_usados.append((pesos, num_pesos))
             return count, pesos_usados
-        except KeyError:
-            # Si el valor no existe en el diccionario, continuar con el cálculo
-            print("Valor no ha sido calculado, prosiguiendo")
+    print("Valor no ha sido calculado, prosiguiendo")
+    
     
     
     denominations.sort(reverse=True)
@@ -96,19 +83,26 @@ while transaccion == 1:
 
     precioP = int(input("Cual es el precio del producto que quiere comprar?: "))
     total = 0
-
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
     billeteQ = int(input("Con que billete se realiza el pago?: "))
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     if checkDenomination(billeteQ, denominaciones):
             total = total + billeteQ
     else:
         print("Porfavor ingrese un billete valido")        
 
     while total <= precioP:
+        
         billeteQ = int(input("Porfavor ingrese otro billete para que su pago sea mas que el total: "))
         if checkDenomination(billeteQ, denominaciones):
             total = total + billeteQ
             print("Total de pesos ingresados: ", total)
+            print("----------------------------------------")
+            print("Total de costo del producto: ", precioP)
+            print("----------------------------------------")
+            print("Dinero necesario: ", precioP-total)
+            print("\n\n\n")
         else:
             print("Porfavor ingrese un billete valido")
         
@@ -116,9 +110,11 @@ while transaccion == 1:
         print("No hay cambio" )
     else:
         cambioNecesario = total - precioP
+        print("\n\n\n\n\n\n\n\n")
         # No vamos a modificar la funcion proporcionada en al diapositiva, solo agregamos la parte de revisar si el calculo existe
         # Para este punto vamos a estar seguros que la cantidad de billetes proporcionados es suficiente para realizar el pago    
         total_pesos, pesos_usados = min_pesos(cambioNecesario, denominaciones, cambio)
+        print("Cambio total de: ", cambioNecesario, " $")
         print(f"Número mínimo de monedas/billetes necesarios: {total_pesos}")
         print("Monedas/Billetes usados:")
         for pesos, count in pesos_usados:
